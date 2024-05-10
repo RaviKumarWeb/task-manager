84c07128ce0f6b39f15de7cd7b9ee86e401c9cfe
Ravi Kumar <137670687+RaviKumarWeb@users.noreply.github.com>
2024-05-10 12:30:43 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -95,7 +95,7 @@ const Login = () => {
                 className=" w-full h-10 bg-blue-700 text-white"
               />
 
-              <p className="text-center text-red-500 text-xl mt-3">Use Google Login These Functionality Not Work</p>
+              <p className="text-center text-red-500 text-[14px] mt-3">Use Google Login These Functionality Not Work</p>
 
               
             </div>
